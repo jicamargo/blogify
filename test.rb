33bbe2859abf3edfdd1568c0ceb7e4354e638862
jic <ji_camargo@yahoo.com>
2023-08-23 16:20:43 -0500

    update models, add foreing keys

---
+++ test.rb
@@ -0,0 +1,8 @@
+
+class User < ApplicationRecord
+end
+
+User.create(first_name: 'Tom', last_name: 'Smith', age: '37') # Create
+User.find_by(first_name: 'Tom') # Read
+User.find(1).update(first_name: 'Ann') # Update
+User.find_by(first_name: 'Tom').delete # Delete
