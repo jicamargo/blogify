055358eaeb7aa869dd02fc9abb2677aa3c931b1d
jic <ji_camargo@yahoo.com>
2023-08-23 17:31:17 -0500

    add custom methods to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
+
+  #  method that updates the comments counter for a post.
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  # custom method to return the three most recent posts of this user
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+  end
+
+  # method that updates the posts counter for this user.
+  def update_posts_counter
+    update(posts_counter: posts.count)
+  end
 end
