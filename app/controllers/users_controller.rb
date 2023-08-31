5cbc9e9b35bb7c04be661ac2d72e89e7a534c22c
jic <ji_camargo@yahoo.com>
2023-08-31 13:23:55 -0500

    update style.css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #update the posts counter for each user
+    # update the posts counter for each user
     @user.update_posts_counter
-    #update the comments counter for each post
-    #update the likes counter for each post
+    # update the comments counter for each post
+    # update the likes counter for each post
     @user.posts.each do |post|
       post.comments.each(&:update_comments_counter)
       post.likes.each(&:update_likes_counter)
