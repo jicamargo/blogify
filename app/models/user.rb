a6944e376c56818c5b2ab2dab6e507003697a03a
jic <ji_camargo@yahoo.com>
2023-09-06 17:10:13 -0500

    fix Rubocop linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   before_create :set_default_values
-  
+
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
