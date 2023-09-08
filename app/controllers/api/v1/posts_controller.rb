8f4f228d5c2aef8bcc0f59c42c54e62deb96af4e
jic <ji_camargo@yahoo.com>
2023-09-07 21:20:33 -0500

    add routes to API for posts, add app/controller/api/v1/post_controller.rb to handle api request

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :set_user
+
+  # GET /api/v1/users/:user_id/posts
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
