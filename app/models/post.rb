1f41d946d0147abe318251e6f8b364fb1c7c7ba9
jic <ji_camargo@yahoo.com>
2023-11-06 20:31:45 -0500

    add rich text feature: - add gems - install text_action -run db:migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+  has_rich_text :text
 
   # custom method which returns the 5 most recent comments for a given post.
   def recent_comments(limit = 5)
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,16 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'shoulda/matchers'
-
-# ENV['RAILS_ENV'] ||= 'test'
-ENV['RAILS_ENV'] = 'test'
+ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-# Add additional requires below this line. Rails is not loaded until this point!
+abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# require 'factory_bot_rails'
-# require 'factory_bot'
+# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -36,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -65,8 +60,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
 end
-
-Capybara.default_driver = :selenium_chrome
